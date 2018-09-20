4bf93eefaedd72789df63d4f3ee35a1691b0d964
Pdbz199 <279pdbz199@gmail.com>
2018-09-19 21:44:17 -0400

    Basic UI update

--- BetterMenuApp/App.js
+++ BetterMenuApp/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import { HomeScreen } from './screens/home_screen';
 
-export default class App extends React.Component {
+class DetailsScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+            title="Go back"
+            onPress={() => this.props.navigation.goBack()}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
   },
-});
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
+export default App
